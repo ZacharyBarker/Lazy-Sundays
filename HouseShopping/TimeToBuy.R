80afd6a2093adeeddacef3d1e1e6dce89bf34e7e
Zachary Barker <Zachary Barker>
2017-01-14 10:58:28 -0500

    started a script to analyze when to buy a house

---
+++ HouseShopping/TimeToBuy.R
@@ -0,0 +1,19 @@
+################################################################################
+#
+# Time to buy analysis
+#
+# Zachary Barker, January 2017
+#
+################################################################################
+
+# House terms
+HousePrice = 200000
+DownPayment = 20000
+InterestRate = 0.04
+TaxRate = 0.013
+MortgageLength = 15
+
+Rent = 600
+Roommate = 400
+
+
