adb2fec8bf52d0a3c885eda4b5884802b540e1bd
Zachary Barker <Zachary Barker>
2017-01-14 16:21:13 -0500

    fixed time series population

--- HouseShopping/TimeToBuy.R
+++ HouseShopping/TimeToBuy.R
@@ -6,6 +6,9 @@
 #
 ################################################################################
 
+# Import libraries
+library(ggplot2)
+
 # House terms
 HousePrice <- 200000
 DownPayment <- 20000
@@ -20,9 +23,9 @@ Roommate <- 400
 # Calculated monthly payments following the equation:
 #    P <- (Pv*R) / [1 - (1 + R)^(-n)] 
 MonthlyEffectiveInterest <- (InterestRate + TaxRate)/12
-MonthlyPayment <- (HousePrice - DownPayment)
-     *(MonthlyEffectiveInterest*((1+ MonthlyEffectiveInterest)^(MortgageLength*12)))
-     /((1+MonthlyEffectiveInterest)^(MortgageLength*12)-1)
+MonthlyPayment <- (HousePrice - DownPayment)*
+     (MonthlyEffectiveInterest*((1+ MonthlyEffectiveInterest)^(MortgageLength*12)))/
+     ((1+MonthlyEffectiveInterest)^(MortgageLength*12)-1)
 
 # Calculate time series
 month <- c(1:360)
@@ -31,19 +34,21 @@ df$CumulativeAppartment <- df$month*Rent
 df$CumulativeHouse <- df$month*MonthlyPayment
 
 # Calculate first row 
-df$MonthlyInterest[1] <- (HousePrice-DownPayment)*MonthlyEffectiveInterest
+df$MonthlyInterest[1] <-(HousePrice-DownPayment)*MonthlyEffectiveInterest
 df$MonthlyEquity[1] <- MonthlyPayment - df$MonthlyInterest[1]
 df$CumulativeInterest[1] <- df$MonthlyInterest[1]
 df$CumulativeEquity[1] <- df$MonthlyEquity[1]
 
+# Calculate remainder of timeseries 
 for(i in 2:360){
      df$MonthlyInterest[i] <- MonthlyEffectiveInterest*(HousePrice-DownPayment-df$CumulativeEquity[i-1])
      df$MonthlyEquity[i] <- MonthlyPayment - df$MonthlyInterest[i]
-     df$CumulativeInterest[i] <- df$MonthlyInterest[i] + df$MonthlyInterest[i-1]
-     df$CumulativeEquity[i] <- df$MonthlyEquity[i] + df$MonthlyEquity[i-1]
+     df$CumulativeInterest[i] <- df$MonthlyInterest[i] + df$CumulativeInterest[i-1]
+     df$CumulativeEquity[i] <- df$MonthlyEquity[i] + df$CumulativeEquity[i-1]
 }
 
 
+# Plot
 
 
 
