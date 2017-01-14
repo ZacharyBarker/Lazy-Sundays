511bb1f8fd562a6f2348d15fe76d719b47d59784
Zachary Barker <Zachary Barker>
2017-01-14 11:24:49 -0500

    added monthly calcs

--- HouseShopping/TimeToBuy.R
+++ HouseShopping/TimeToBuy.R
@@ -17,3 +17,24 @@ Rent = 600
 Roommate = 400
 
 
+# Calculated monthly payments following the equation:
+#
+#    P = (Pv*R) / [1 - (1 + R)^(-n)] 
+#
+#    where:
+#
+#    Pv  = Present Value (beginning value or amount of loan)
+#    APR = Annual Percentage Rate (one year time period)
+#    R  = Periodic Interest Rate = APR/ # of interest periods per year
+#    P   = Monthly Payment
+#    n   = # of interest periods for overall time period (i.e., interest
+#      periods per year * number of years)
+
+MonthlyEffectiveInterest = (InterestRate + TaxRate)/12
+MonthlyPayment = (HousePrice - DownPayment)
+     *(MonthlyEffectiveInterest*((1+ MonthlyEffectiveInterest)^(MortgageLength*12)))
+     /((1+MonthlyEffectiveInterest)^(MortgageLength*12)-1)
+
+
+
+
